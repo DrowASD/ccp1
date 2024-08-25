24126b22ab814a044be0fa20c7c070608ac7ffb6
DrowASD <drowasdfg@gmail.com>
2024-08-25 10:14:02 +0300

    Добавьте файлы проекта.

---
+++ J2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <memory>
+
+int main() {
+    std::unique_ptr<int> ptr(new int(5));
+
+    std::cout << *ptr << std::endl; // Выводит 5
+
+    *ptr = 10;
+
+    std::cout << *ptr << std::endl; // Выводит 10
+
+    return 0;
+}
